eec1ef356a537053cd39532592b632feac6d7952
Xingyu Wang <kara.wang@mail.utoronto.ca>
2018-02-03 11:42:58 -0500

    Modified ExpenseManager
    For preparing creating subclasses

--- green/app/src/main/java/ellehacks2018/green/model/Expense.java
+++ green/app/src/main/java/ellehacks2018/green/model/Expense.java
@@ -1,4 +1,4 @@
-package com.example.model;
+package ellehacks2018.green.model;
 
 /**
  * Created by karally on 2018/2/3.
@@ -17,64 +17,43 @@ public class Expense implements Serializable{
 
     private FileManager fileManager = new FileManager();
 
-    private static ArrayList<Expense> expenses = new ArrayList<>();
-
     private int amount;
 
     private String name;
 
     private Date date;
 
-    public Expense(int amount, String name, Date date) {
-        if (amount == 0){
-            throw new IllegalArgumentException("Amount can't be 0!");
-        }
-        if (name == ""){
-            throw new IllegalArgumentException("Name cannot be blank!");
-        }
-        if (date.after(Calendar.getInstance().getTime())){
-            throw new IllegalArgumentException("You're not a time traveller = =");
-        }
-        this.amount = amount;
-        this.name = name;
-        this.date = date;
-    }
+    private String catagory;
 
-    public int getAmount() {
-        return amount;
+    public Expense(int amount, String name, Date date, String catagory) {
+        setAmount(amount);
+        setName(name);
+        setDate(date);
+        setCatagory(catagory);
     }
 
     public void setAmount(int amount) {
+        if (amount == 0){
+            throw new IllegalArgumentException("Amount can't be 0!");
+        }
         this.amount = amount;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
+        if (name == ""){
+            throw new IllegalArgumentException("Name cannot be blank!");
+        }
         this.name = name;
     }
 
-    public Date getDate() {
-        return date;
-    }
-
     public void setDate(Date date) {
+        if (date.after(Calendar.getInstance().getTime())){
+            throw new IllegalArgumentException("You're not a time traveller = =");
+        }
         this.date = date;
     }
 
-    public void save(String filePath) throws IOException {
-        ObjectOutput obj = fileManager.saveToFile(filePath);
-
-        obj.writeObject(expenses);
-        obj.close();
+    public void setCatagory(String catagory) {
+        this.catagory = catagory;
     }
-
-    public void restore(String filePath) throws IOException {
-        ObjectOutput output = fileManager.saveToFile(filePath);
-        output.writeObject(expenses);
-        output.close();
-    }
-
 }
